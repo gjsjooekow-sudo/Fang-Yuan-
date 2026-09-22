745274e10d2e0fae0752f22d0f18901bc2d01f1e
gjsjooekow-sudo <gjsjooekow@gmail.com>
2026-09-22 20:24:08 +0100

    Update fork.js

--- scripts/cmds/fork.js
+++ scripts/cmds/fork.js
@@ -30,10 +30,10 @@ exports.onChat = async function({event: z, api: y}){
   if(fork){
     y.sendMessage(
 `🔗𝗚𝗶𝘁𝗛𝘂𝗯 𝗙𝗼𝗿𝗸 𝗟𝗶𝗻𝗸:
-https://github.com/EryXenX/GOAT-MESSENGER.git
+ز'بي
 
 🎬 𝗦𝗲𝘁𝘂𝗽 𝗧𝘂𝘁𝗼𝗿𝗶𝗮𝗹👇🏼
-https://youtu.be/gPf_BFhQz_w?si=T1N6sB2DefeTGq2R`,
+ز'بي`,
       t,
       z.messageID
     );
